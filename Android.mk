ab39b1e3bb3663b793ad4822ac0e98bfdfc9c3c9
HinohArata <mmgcntk@gmail.com>
2024-05-06 13:12:02 +0700

    launcher: Initial commits

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Launcher3
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := Launcher3QuickStep.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_OVERRIDES_PACKAGES := AfterHome AfterHomeGo AfterHomeQuickStep AfterHomeQuickStepGo
+include $(BUILD_PREBUILT)
